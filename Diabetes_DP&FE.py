ebada18ae520c60cb340ee810c088fe817bf2a5a
alibaltaci <alibaltaci13@gmail.com>
2021-02-16 15:23:48 +0300

    Missing Values Analysis

--- Diabetes_DP&FE.py
+++ Diabetes_DP&FE.py
@@ -33,3 +33,42 @@ hf.has_outliers(df,df.columns)
 
 
 # MISSING VALUES ANALYSIS
+
+df.isnull().values.any() # False --> ?
+
+# It is not possible to have a value of 0 in the some variables in the data set.
+# Now let's query the value of 0 in variables.
+
+hf.num_catcher(df,0)
+
+# Variable  Pregnancies : 111
+# Variable  Glucose : 5
+# Variable  BloodPressure : 35
+# Variable  SkinThickness : 227
+# Variable  Insulin : 374
+# Variable  BMI : 11
+# Variable  Outcome : 500
+
+# Missing values may be replaced by 0.
+
+cols_na = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
+
+for col in cols_na:
+    df.loc[(df[col] == 0), col] = np.NaN
+
+# an other way (with lambda)
+
+# for col in variables_with_na:
+#     df[col] = df[col].apply(lambda x: np.NaN if x == 0 else x)
+
+# Checking missing values again
+
+df.isnull().values.any()
+df.isnull().sum().sort_values()
+
+hf.num_catcher(df,0)
+
+# Variable  Pregnancies : 111
+# Variable  Outcome : 500
+
+# these variables can have values of 0.
