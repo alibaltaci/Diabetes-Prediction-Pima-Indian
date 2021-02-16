bfd21f5fa860f93772123e0bfdf41db2ced28e35
alibaltaci <alibaltaci13@gmail.com>
2021-02-16 14:16:24 +0300

    Outlier Analysis

--- Diabetes_DP&FE.py
+++ Diabetes_DP&FE.py
@@ -13,3 +13,23 @@ import helper_functions as hf
 warnings.simplefilter(action="ignore")
 pd.pandas.set_option('display.max_columns', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+
+# Load the dataset
+
+df_backup = pd.read_csv(r"C:\Users\TOSHIBA\Desktop\Diabetes Pima Indian\diabetes.csv")
+df = df_backup.copy()
+df.head()
+
+# OUTLIER ANALYSIS
+
+hf.has_outliers(df,df.columns)
+
+# Threshold
+for col in df.columns:
+    hf.replace_with_thresholds_with_lambda(df,col)
+
+hf.has_outliers(df,df.columns)
+
+
+# MISSING VALUES ANALYSIS
