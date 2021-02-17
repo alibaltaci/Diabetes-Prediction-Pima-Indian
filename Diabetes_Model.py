c4375b3be401c1f3010dd45d8447da8d5011b8b8
alibaltaci <alibaltaci13@gmail.com>
2021-02-17 15:43:18 +0300

    LightGBM Model Tuning

--- Diabetes_Model.py
+++ Diabetes_Model.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import helper_functions as hf
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
@@ -73,19 +74,47 @@ rf_params = {"n_estimators": [100, 200, 500, 1000],
 
 rf_model = RandomForestClassifier(random_state=42)
 
-gs_cv = GridSearchCV(rf_model, rf_params, cv=10, n_jobs=-1, verbose=2).fit(X, y)
+rf_gscv = GridSearchCV(rf_model, rf_params, cv=10, n_jobs=-1, verbose=2).fit(X, y)
 # [Parallel(n_jobs=-1)]: Done 1920 out of 1920 | elapsed: 16.8min finished
 
-gs_cv.best_params_  # {'max_depth': 8,
+rf_gscv.best_params_  # {'max_depth': 8,
                     # 'max_features': 7,
                     # 'min_samples_split': 5,
                     # 'n_estimators': 500}
 
 # Final Model
-rf_tuned = RandomForestClassifier(**gs_cv.best_params_).fit(X,y)
+rf_tuned = RandomForestClassifier(**rf_gscv.best_params_).fit(X,y)
 
 cross_val_score(rf_tuned, X, y, cv=10).mean()  # 0.89
 
 # Feature Importances
 feature_imp_rf = pd.Series(rf_tuned.feature_importances_, index=X.columns).sort_values(ascending=False)
+
+hf.plot_feature_importances(feature_imp_rf)
+
+
+# LightGBM  Model Tuning #
+
+lgbm_params = {"learning_rate": [0.01, 0.03, 0.05, 0.1, 0.5],
+               "n_estimators": [500, 1000, 1500],
+               "max_depth": [3, 5, 8]}
+
+lgbm_model = LGBMClassifier(random_state=42)
+
+rf_gscv = GridSearchCV(lgbm_model, lgbm_params, cv=10, n_jobs=-1, verbose=2).fit(X, y)
+# [Parallel(n_jobs=-1)]: Done 450 out of 450 | elapsed:  1.6min finished
+
+lgbm_gscv.best_params_
+
+# Final Model
+lgbm_tuned = LGBMClassifier(**rf_gscv.best_params_).fit(X, y)
+
+cross_val_score(lgbm_tuned, X, y, cv=10).mean()   # 0.8997
+
+# Feature Importances
+feature_imp_lgbm = pd.Series(lgbm_tuned.feature_importances_, index=X.columns).sort_values(ascending=False)
+
+hf.plot_feature_importances(feature_imp_lgbm)
+
+
 
